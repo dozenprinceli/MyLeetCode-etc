d0a309ed2cfc76c6325f24a6aa5f6664110d3999
dozenprinceli <dozenprinceli@gmail.com>
2022-08-20 02:04:54 +0800

    Problem.442 first commit

---
+++ LeetCode/src/leetcode/editor/cn/[442]数组中重复的数据.java
@@ -0,0 +1,64 @@
+//给你一个长度为 n 的整数数组 nums ，其中 nums 的所有整数都在范围 [1, n] 内，且每个整数出现 一次 或 两次 。请你找出所有出现 两次 
+//的整数，并以数组形式返回。 
+//
+// 你必须设计并实现一个时间复杂度为 O(n) 且仅使用常量额外空间的算法解决此问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [4,3,2,7,8,2,3,1]
+//输出：[2,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,1,2]
+//输出：[1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1]
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// n == nums.length 
+// 1 <= n <= 10⁵ 
+// 1 <= nums[i] <= n 
+// nums 中的每个元素出现 一次 或 两次 
+// 
+//
+// Related Topics 数组 哈希表 👍 653 👎 0
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        Map<Integer, Integer> numFreq = new HashMap<>();
+        for (int num : nums) {
+            numFreq.put(num, numFreq.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> curFreq : numFreq.entrySet()) {
+            if (curFreq.getValue() == 2) {
+                ans.add(curFreq.getKey());
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
