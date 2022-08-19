7b6e3b6fdc45ffef863b527a17e5ac4f88d2ad7d
dozenprinceli <dozenprinceli@gmail.com>
2022-08-20 00:28:31 +0800

    Problem.654 first commit

---
+++ LeetCode/src/leetcode/editor/cn/[654]最大二叉树.java
@@ -0,0 +1,100 @@
+//给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建: 
+//
+// 
+// 创建一个根节点，其值为 nums 中的最大值。 
+// 递归地在最大值 左边 的 子数组前缀上 构建左子树。 
+// 递归地在最大值 右边 的 子数组后缀上 构建右子树。 
+// 
+//
+// 返回 nums 构建的 最大二叉树 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：nums = [3,2,1,6,0,5]
+//输出：[6,3,5,null,2,0,null,null,1]
+//解释：递归调用如下所示：
+//- [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+//    - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+//        - 空数组，无子节点。
+//        - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+//            - 空数组，无子节点。
+//            - 只有一个元素，所以子节点是一个值为 1 的节点。
+//    - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+//        - 只有一个元素，所以子节点是一个值为 0 的节点。
+//        - 空数组，无子节点。
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：nums = [3,2,1]
+//输出：[3,null,2,null,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 1000 
+// 0 <= nums[i] <= 1000 
+// nums 中的所有整数 互不相同 
+// 
+//
+// Related Topics 栈 树 数组 分治 二叉树 单调栈 👍 474 👎 0
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        TreeNode root = null;
+        for (int num : nums) {
+            root = insert(root, num);
+        }
+        return root;
+    }
+
+    private TreeNode insert(TreeNode root, int val) {
+        // 第一个节点, 初始化状态
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        TreeNode cur = new TreeNode(val);
+        // 比左边都大, cur是当前root的父节点
+        if (val > root.val) {
+            cur.left = root;
+            return cur;
+        }
+        // 比当前root小, 位于root的右子树上
+        if (root.right == null || root.right.val < val) { // 右子树为空或cur是右子树root的父节点
+            cur.left = root.right;
+            root.right = cur;
+        } else { // 遍历找到合适位置插入
+            insert(root.right, val);
+        }
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
