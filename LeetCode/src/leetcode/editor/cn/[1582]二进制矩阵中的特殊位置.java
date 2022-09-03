68e9e1a85739b8badfff8262360a5256e8e577b5
dozenprinceli <dozenprinceli@gmail.com>
2022-09-04 00:37:05 +0800

    Problem.172/258/458/1582

---
+++ LeetCode/src/leetcode/editor/cn/[1582]二进制矩阵中的特殊位置.java
@@ -0,0 +1,93 @@
+//给你一个大小为 rows x cols 的矩阵 mat，其中 mat[i][j] 是 0 或 1，请返回 矩阵 mat 中特殊位置的数目 。 
+//
+// 特殊位置 定义：如果 mat[i][j] == 1 并且第 i 行和第 j 列中的所有其他元素均为 0（行和列的下标均 从 0 开始 ），则位置 (i, 
+//j) 被称为特殊位置。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：mat = [[1,0,0],
+//            [0,0,1],
+//            [1,0,0]]
+//输出：1
+//解释：(1,2) 是一个特殊位置，因为 mat[1][2] == 1 且所处的行和列上所有其他元素都是 0
+// 
+//
+// 示例 2： 
+//
+// 输入：mat = [[1,0,0],
+//            [0,1,0],
+//            [0,0,1]]
+//输出：3
+//解释：(0,0), (1,1) 和 (2,2) 都是特殊位置
+// 
+//
+// 示例 3： 
+//
+// 输入：mat = [[0,0,0,1],
+//            [1,0,0,0],
+//            [0,1,1,0],
+//            [0,0,0,0]]
+//输出：2
+// 
+//
+// 示例 4： 
+//
+// 输入：mat = [[0,0,0,0,0],
+//            [1,0,0,0,0],
+//            [0,1,0,0,0],
+//            [0,0,1,0,0],
+//            [0,0,0,1,1]]
+//输出：3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// rows == mat.length 
+// cols == mat[i].length 
+// 1 <= rows, cols <= 100 
+// mat[i][j] 是 0 或 1 
+// 
+//
+// Related Topics 数组 矩阵 👍 30 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int r = mat.length;
+        int c = mat[0].length;
+        int[] colCnt = new int[c]; // 记录每一列拥有的1的数量
+        int[] rowCnt = new int[r]; // 若该行仅有一个1, 则记录1的colIdx, 否则记为-1
+        for (int row = 0; row < r; row++) {
+            boolean hasOne = false;
+            for (int col = 0; col < c; col++) {
+                if (mat[row][col] == 0) {
+                    continue;
+                }
+                if (!hasOne) {
+                    hasOne = true;
+                    rowCnt[row] = col;
+                } else {
+                    rowCnt[row] = -1;
+                }
+                colCnt[col]++;
+            }
+            if (!hasOne) {
+                rowCnt[row] = -1;
+            }
+        }
+        int ans = 0;
+        for (int i : rowCnt) {
+            if (i != -1 && colCnt[i] == 1) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ LeetCode/test/src/bitwise/Bitwise03.java
@@ -0,0 +1,16 @@
+package bitwise;
+
+import org.junit.Test;
+
+/**
+ * @author Dozen Lee<br/>
+ * 2022/9/4 0:12
+ */
+public class Bitwise03 {
+    @Test
+    public void test458() {
+        double d1 = Math.log(125);
+        double d2 = Math.log(5);
+        System.out.println(d1 / d2);
+    }
+}
