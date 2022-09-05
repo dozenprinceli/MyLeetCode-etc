1439cfca1a36724311222aa206c141724bd62b60
dozenprinceli <dozenprinceli@gmail.com>
2022-09-06 00:58:05 +0800

    Problem.828 but TLE

---
+++ LeetCode/test/src/array/Array06.java
@@ -0,0 +1,17 @@
+package array;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Dozen Lee<br/>
+ * 2022/9/6 0:37
+ */
+public class Array06 {
+    @Test
+    public void test() {
+    }
+}
