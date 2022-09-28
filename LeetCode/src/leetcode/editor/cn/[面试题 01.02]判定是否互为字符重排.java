2f2f0d956c0dc9763ea1f63d23f40c9613d22b42
dozenprinceli <dozenprinceli@gmail.com>
2022-09-28 23:34:31 +0800

    Problem01.02

---
+++ LeetCode/src/leetcode/editor/cn/[面试题 01.02]判定是否互为字符重排.java
@@ -0,0 +1,45 @@
+//给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。 
+//
+// 示例 1： 
+//
+// 输入: s1 = "abc", s2 = "bca"
+//输出: true 
+// 
+//
+// 示例 2： 
+//
+// 输入: s1 = "abc", s2 = "bad"
+//输出: false
+// 
+//
+// 说明： 
+//
+// 
+// 0 <= len(s1) <= 100 
+// 0 <= len(s2) <= 100 
+// 
+//
+// Related Topics 哈希表 字符串 排序 👍 80 👎 0
+
+
+import java.util.Arrays;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean CheckPermutation(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        char[] ch1 = s1.toCharArray();
+        char[] ch2 = s2.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        for (int i = 0; i < s1.length(); i++) {
+            if (ch1[i] != ch2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
